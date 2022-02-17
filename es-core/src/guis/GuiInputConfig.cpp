753ae13422ea2e91b5ef74175ebaffdaf500df23
sylph@reset.jp <NullPopPo>
2022-02-17 14:47:30 +0000

    16ボタン対応

--- es-core/src/guis/GuiInputConfig.cpp
+++ es-core/src/guis/GuiInputConfig.cpp
@@ -33,32 +33,33 @@ void GuiInputConfig::initInputConfigStructure()
 {
 	GUI_INPUT_CONFIG_LIST =
 	{
-		{ "a",                false, InputConfig::buttonLabel("a"),    InputConfig::buttonImage("a") },
-		{ "b",                true,  InputConfig::buttonLabel("b"),    InputConfig::buttonImage("b") },
-		{ "x",                true,  "X",    ":/help/buttons_north.svg" },
-		{ "y",                true,  "Y",    ":/help/buttons_west.svg" },
-
-		{ "start",            true,  "START",              ":/help/button_start.svg" },
-		{ "select",           true,  "SELECT",             ":/help/button_select.svg" },
-
 		{ "up",               false, "UP",           ":/help/dpad_up.svg" },
 		{ "down",             false, "DOWN",         ":/help/dpad_down.svg" },
 		{ "left",             false, "LEFT",         ":/help/dpad_left.svg" },
 		{ "right",            false, "RIGHT",        ":/help/dpad_right.svg" },
 
-		{ "pageup",          true,  "L1",      ":/help/button_l.svg" },
-		{ "pagedown",        true,  "R1",     ":/help/button_r.svg" },
+		{ "a",                false, InputConfig::buttonLabel("a"),    InputConfig::buttonImage("a") },
+		{ "b",                true,  InputConfig::buttonLabel("b"),    InputConfig::buttonImage("b") },
+		{ "c",                true,  "C",    ":/help/button_c.svg" }, // NullPopPo
+		{ "x",                true,  "X",    ":/help/buttons_north.svg" },
+		{ "y",                true,  "Y",    ":/help/buttons_west.svg" },
+		{ "z",                true,  "Z",    ":/help/button_z.svg" }, // NullPopPo
+
+		{ "start",            true,  "START",              ":/help/button_start.svg" },
+		{ "select",           true,  "SELECT",             ":/help/button_select.svg" },
 
 		{ "joystick1up",     true,  "LEFT ANALOG UP",     ":/help/analog_up.svg" },
 		{ "joystick1left",   true,  "LEFT ANALOG LEFT",   ":/help/analog_left.svg" },
 		{ "joystick2up",     true,  "RIGHT ANALOG UP",     ":/help/analog_up.svg" },
 		{ "joystick2left",   true,  "RIGHT ANALOG LEFT",   ":/help/analog_left.svg" },
 
+		{ "pageup",          true,  "L1",      ":/help/button_l.svg" },
+		{ "pagedown",        true,  "R1",     ":/help/button_r.svg" },
 		{ "l2",              true,  "L2",       ":/help/button_lt.svg" },
 		{ "r2",              true,  "R2",      ":/help/button_rt.svg" },
 		{ "l3",              true,  "L3",       ":/help/analog_thumb.svg" },
 		{ "r3",              true,  "R3",      ":/help/analog_thumb.svg" },
-
+ 		{ "menu",            true,  "MENU",      ":/help/button_menu.svg" }, // NullPopPo 
 		{ "hotkey",          true,  "HOTKEY",      ":/help/button_hotkey.svg" } // batocera
 	};
 }
