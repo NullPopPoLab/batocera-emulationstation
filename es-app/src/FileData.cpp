35c4abedf1181c63828ab2a24f27071fa4e0d96b
sylph@reset.jp <NullPopPo>
2022-02-18 00:58:19 +0000

    m3u拡張定義部分の読み飛ばし

--- es-app/src/FileData.cpp
+++ es-app/src/FileData.cpp
@@ -798,6 +798,13 @@ std::set<std::string> FileData::getContentFiles()
 					if (trim[0] == '#' || trim[0] == '\\' || trim[0] == '/')
 						continue;
 
+					if(trim[0]=='*'){
+						// skip Advanced M3U attribute 
+						auto ep=trim.find(';');
+						if(ep==std::string::npos)continue;
+						if(trim.size()<=ep+1)continue;
+						trim=trim.substr(ep+1);
+					}
 					files.insert(path + "/" + trim);
 				}
 
